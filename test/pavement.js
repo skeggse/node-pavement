9ec0a7ae2a29f8b6a6d760c333fab74ec8672e1e
Eli Skeggs <skeggse@gmail.com>
2014-02-27 10:23:48 -0800

    Remove test logging
    Signed-off-by: Eli Skeggs <skeggse@gmail.com>

--- test/pavement.js
+++ test/pavement.js
@@ -5,8 +5,6 @@ describe('pavement', function() {
 
   it('should compile an empty getter', function() {
     var get = pave();
-    console.log('empty');
-    console.log(get + '');
     expect(get()).to.equal(undefined);
     expect(get(null)).to.equal(null);
     expect(get(false)).to.equal(false);
@@ -15,8 +13,6 @@ describe('pavement', function() {
 
   it('should compile a small getter', function() {
     var get = pave('one');
-    console.log('small');
-    console.log(get + '');
     expect(get()).to.equal(undefined);
     expect(get({})).to.equal(undefined);
     expect(get({wan: false})).to.equal(undefined);
@@ -26,8 +22,6 @@ describe('pavement', function() {
 
   it('should compile a deep getter', function() {
     var get = pave('one', 'two', 'three');
-    console.log('deep');
-    console.log(get + '');
     expect(get()).to.equal(undefined);
     expect(get({})).to.equal(undefined);
     expect(get({one: null})).to.equal(undefined);
@@ -44,8 +38,6 @@ describe('pavement', function() {
 
   it('should compile an index getter', function() {
     var get = pave(0);
-    console.log('index');
-    console.log(get + '');
     expect(get()).to.equal(undefined);
     expect(get({})).to.equal(undefined);
     expect(get([])).to.equal(undefined);
@@ -59,8 +51,6 @@ describe('pavement', function() {
 
   it('should compile a deep-index getter', function() {
     var get = pave(0, 1, 2, 3);
-    console.log('deep-index');
-    console.log(get + '');
     expect(get()).to.equal(undefined);
     expect(get({})).to.equal(undefined);
     expect(get([])).to.equal(undefined);
@@ -80,22 +70,16 @@ describe('pavement', function() {
 
   it('should compile a deep-aa getter', function() {
     var get = pave('one', 2, 'three', 4, 'five');
-    console.log('deep-aa');
-    console.log(get + '');
     expect(get({one: [1, 1, {three: [1, 1, 1, 1, {five: ref}]}]})).to.equal(ref);
   });
 
   it('should compile a deep-ab getter', function() {
     var get = pave('one', 2, 'three', 4);
-    console.log('deep-ab');
-    console.log(get + '');
     expect(get({one: [1, 1, {three: [1, 1, 1, 1, ref]}]})).to.equal(ref);
   });
 
   it('should compile a deep-ba getter', function() {
     var get = pave(0, 'one', 2, 'three', 4, 'five');
-    console.log('deep-ba');
-    console.log(get + '');
     expect(get()).to.equal(undefined);
     expect(get({})).to.equal(undefined);
     expect(get([])).to.equal(undefined);
@@ -105,8 +89,6 @@ describe('pavement', function() {
 
   it('should compile a deep-bb getter', function() {
     var get = pave(0, 'one', 2, 'three', 4);
-    console.log('deep-bb');
-    console.log(get + '');
     expect(get()).to.equal(undefined);
     expect(get({})).to.equal(undefined);
     expect(get([])).to.equal(undefined);
@@ -116,8 +98,6 @@ describe('pavement', function() {
 
   it('should compile a complex getter', function() {
     var get = pave('one', -1, 'two', 'three', 4, -1, 5, 'seven');
-    console.log('complex');
-    console.log(get + '');
     expect(get()).to.equal(undefined);
     expect(get({})).to.equal(undefined);
     expect(get({one: [{two: {three: [1, 1, 1, 1, [[1, 1, 1, 1, 1, {seven: ref}]]]}}]})).to.equal(ref);
@@ -128,8 +108,6 @@ describe('pavement', function() {
 
   it('should compile a deep-loop getter', function() {
     var get = pave(-1, -1, 'one');
-    console.log('deep-loop');
-    console.log(get + '');
     expect(get()).to.equal(undefined);
     expect(get([[{one: ref}]])).to.equal(ref);
     expect(get([[{two: 1}, {one: ref}]])).to.equal(ref);
@@ -139,8 +117,6 @@ describe('pavement', function() {
 
   it('should compile a deeper-loop getter', function() {
     var get = pave(-1, -1, -1, 'one');
-    console.log('deeper-loop');
-    console.log(get + '');
     expect(get()).to.equal(undefined);
     expect(get([[[{one: ref}]]])).to.equal(ref);
     expect(get([[[,,],[,,],[,,]],[[,,],[,,],[,,]],[[,,],[,,],[,,]]])).to.equal(undefined);
@@ -151,8 +127,6 @@ describe('pavement', function() {
 
   it('should compile a special getter', function() {
     var get = pave('one', -1);
-    console.log('special');
-    console.log(get + '');
     expect(get()).to.equal(undefined);
     expect(get({})).to.equal(undefined);
     expect(get({one: [ref]})).to.equal(ref);
